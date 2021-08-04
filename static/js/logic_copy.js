090fc3c5065af05300b00dddbcc8b7b1067f661b
adriana-icasiano <81338603+adriana-icasiano@users.noreply.github.com>
2021-08-04 07:37:14 -0400

    color based on depth but colors don't work

--- static/js/logic_copy.js
+++ static/js/logic_copy.js
@@ -1,6 +1,17 @@
-// A function to determine the marker size based on the population
+// A function to determine the marker size based on the magnitude
 function markerSize(magnitude) {
-  return Math.sqrt(magnitude) * 50;
+  return Math.sqrt(magnitude) * 30000;
+}
+
+// A function to determine the marker size based on the magnitude
+function getColor(depth) {
+  return (depth) > 90 ? 'rgb(232,0,0':
+         (depth) <= 90 && (depth) > 70 ? 'dark orange':
+         (depth) <= 70 && (depth) > 50? 'orange':
+         (depth) <= 50 && (depth) > 30 ? 'light orange':
+         (depth) <= 30 && (depth) >= 10 ? 'yellow':
+         (depth) < 10 ? 'chartreuse':
+         'chartreuse';
 }
 
 // Store our API endpoint as queryUrl.
@@ -10,22 +21,30 @@ let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 d3.json(queryUrl).then(function (data) {
 
   let earthquake = data.features; 
-
-// Define arrays to hold the created city and state markers.
+  
+  console.log(earthquake);
+  
+// Define arrays to hold the created earthquake markers.
 var earthquakeMarkers = [];
 
 
-// Loop through locations, and create the city and state markers.
+// Loop through locations, and create the earthquake markers.
 for (var i = 0; i < earthquake.length; i++) {
-  // Setting the marker radius for the state by passing population into the markerSize function
+  // Setting the marker radius for the state by passing magnitude into the markerSize function
+  // console.log(earthquake[i].properties.mag);
+  
   earthquakeMarkers.push(
-    L.circle(earthquake[i].properties.coordinates, {
+    L.circle([earthquake[i].geometry.coordinates[1], earthquake[i].geometry.coordinates[0]], {
       stroke: false,
       fillOpacity: 0.75,
-      color: "red",
-      fillColor: "red",
-      radius: markerSize(earthquake[i].properties.mag)
-    })
+      stroke: true,
+      color: "black",
+      fillColor: getColor(earthquake[i].geometry.coordinates[2]),
+      weight: .5,
+      radius: markerSize(parseInt(earthquake[i].properties.mag))
+    }).bindPopup(`<h3>${earthquake[i].properties.title}</h3> <hr>
+    <h3> Magnitude: ${earthquake[i].properties.mag} </h3>
+    <h3> Depth: ${earthquake[i].geometry.coordinates[2]} </h3>`)
   );
 
 }
@@ -39,7 +58,7 @@ var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 
-// Create two separate layer groups: one for the city markers and another for the state markers.
+// Create two separate layer groups: one for the earthquake markers.
 var earthquakes = L.layerGroup(earthquakeMarkers);
 
 // Create a baseMaps object.
