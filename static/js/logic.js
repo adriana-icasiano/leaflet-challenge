3c0856686857e4e93d22054e00c15e3e3830c6af
adriana-icasiano <81338603+adriana-icasiano@users.noreply.github.com>
2021-08-05 07:24:03 -0400

    added legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,62 +1,121 @@
+function getRadius(magnitude_rad) {
+  if (magnitude_rad === 0) {
+    return 1;
+    }
+  return magnitude_rad *30000;
+  }
+
+// A function to determine the marker size based on the magnitude
+function markerSize(magnitude) {
+  return Math.sqrt(magnitude) * 30000;
+}
+
+// A function to determine the marker size based on the magnitude
+function getColor(depth) {
+  return (depth) > 90 ? "rgb(232,0,0)": //red
+         (depth) <= 90 && (depth) > 70 ? "rgb(255, 98, 0)": //dark orange
+         (depth) <= 70 && (depth) > 50? "rgb(255, 142, 0)": // light orange
+         (depth) <= 50 && (depth) > 30 ? "rgb(255, 181, 0)": //orange
+         (depth) <= 30 && (depth) >= 10 ? "rgb(255, 255, 0)": //yellow
+         (depth) < 10 ? "rgb(0, 235, 0)": //light green
+         "rgb(0, 235, 0)"; //light green
+}
+
 // Store our API endpoint as queryUrl.
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
-});
 
-function createFeatures(earthquakeData) {
+  let earthquake = data.features; 
+  
+  console.log(earthquake);
+  
+// Define arrays to hold the created earthquake markers.
+var earthquakeMarkers = [];
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+// Loop through locations, and create the earthquake markers.
+for (var i = 0; i < earthquake.length; i++) {
+  // Setting the marker radius for the state by passing magnitude into the markerSize function
+  magnitude_rad = earthquake[i].properties.mag;
+  
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
+  earthquakeMarkers.push(
+    L.circle([earthquake[i].geometry.coordinates[1], earthquake[i].geometry.coordinates[0]], {
+      stroke: false,
+      fillOpacity: 0.75,
+      stroke: true,                                                                                                                                                                                                                                    
+      color: "black",
+      fillColor: getColor(earthquake[i].geometry.coordinates[2]),
+      weight: .5,
+      radius: getRadius(magnitude_rad),
+    }).bindPopup(`<h3>${earthquake[i].properties.title}</h3> <hr>
+    <h3> Magnitude: ${earthquake[i].properties.mag} </h3>
+    <h3> Depth: ${earthquake[i].geometry.coordinates[2]} </h3>`)
+  );
+
 }
 
-function createMap(earthquakes) {
+// Create the base layers.
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
 
-    // Create the base layers.
-    var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    })
-  
-    // Create a baseMaps object.
-    var baseMaps = {
-      "Street Map": street
-    };
-  
-    // Create an overlay object to hold our overlay.
-    var overlayMaps = {
-      Earthquakes: earthquakes
-    };
-  
-    // Create our map, giving it the streetmap and earthquakes layers to display on load.
-    var myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 5,
-      layers: [street, earthquakes]
-    });
-  
-    // Create a layer control.
-    // Pass it our baseMaps and overlayMaps.
-    // Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-      collapsed: false
-    }).addTo(myMap);
-  
-  }
+var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+// Create two separate layer groups: one for the earthquake markers.
+var earthquakes = L.layerGroup(earthquakeMarkers);
+
+// Create a baseMaps object.
+var baseMaps = {
+  "Street Map": street,
+  "Topographic Map": topo
+};
+
+// Create an overlay object.
+var overlayMaps = {
+  "Earthquakes": earthquakes,
+
+};
+
+// Define a map object.
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [street, earthquakes]
+});
+
+// Pass our map layers to our layer control.
+// Add the layer control to the map.
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        depthOfQuake = [0, 10, 30, 50, 70, 90],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < depthOfQuake.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(depthOfQuake[i] + 1) + '"></i> ' +
+            depthOfQuake[i] + (depthOfQuake[i + 1] ? '&ndash;' + depthOfQuake[i + 1] + '<br><br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+
+});
+
   
